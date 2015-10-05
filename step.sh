b2af1c9483516d485cc7079c484305b2b3133c8e
Tamás Bazsonyi <tamas.bazsonyi@gmail.com>
2015-10-05 16:12:05 +0200

    Updated to use GIT_DIRECTORY if added

--- step.sh
+++ step.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ ! -z "${dir_to_check}" ] ; then
+  echo "Changing directory to:"
+  echo "${dir_to_check}"
+  cd ${dir_to_check}
+fi
 
 echo "Searching for changes in tracked files..."
 modified_files="$(git status --porcelain)"
